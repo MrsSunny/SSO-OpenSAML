9c34ca369ec76acaa8050070956908da924c6138
sunny <lzx@ciphergateway.com>
2016-03-12 11:20:11 +0800

    add service and controller for login

--- src/main/java/org/sms/project/login/controller/LoginController.java
+++ src/main/java/org/sms/project/login/controller/LoginController.java
@@ -1,7 +1,6 @@
 package org.sms.project.login.controller;
 
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,14 +15,5 @@ public class LoginController {
   @RequestMapping(value = "")
   public String add(Model model, HttpServletRequest request) {
     return "login/login";
-  }
-  
-  @RequestMapping(value = "/error")
-  public String error(Model model, HttpServletRequest request) {
-//    throw new RuntimeException("错误处理");
-    String a = "sadfasdfas";
-    int s = Integer.parseInt(a);
-    System.out.println(s);
-    return "/login/login_success";
   }
 }
