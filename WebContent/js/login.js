0b90c5d18fa82fe0375021521ee6b8fd6652706e
sunny <lzx@ciphergateway.com>
2016-03-31 11:19:07 +0800

    delete the unused page

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,8 +1,9 @@
 var Login = function() {
+//	var xx = $.getUrlParam('error');
+	alert("sdfsd");
 	return {
 		init : function() {
-			$('.login-form')
-					.validate(
+			$('.login-form').validate(
 							{
 								errorElement : 'label', // default input error
 								errorClass : 'help-inline', // default input
@@ -111,85 +112,6 @@ var Login = function() {
 				jQuery('.login-form').show();
 				jQuery('.forget-form').hide();
 			});
-			$('.register-form')
-					.validate(
-							{
-								errorElement : 'label', // default input error
-														// message container
-								errorClass : 'help-inline', // default input
-															// error message
-															// class
-								focusInvalid : false, // do not focus the last
-														// invalid input
-								ignore : "",
-								rules : {
-									username : {
-										required : true
-									},
-									password : {
-										required : true
-									},
-									rpassword : {
-										equalTo : "#register_password"
-									},
-									email : {
-										required : true,
-										email : true
-									},
-									tnc : {
-										required : true
-									}
-								},
-								messages : { // custom messages for radio
-												// buttons and checkboxes
-									tnc : {
-										required : "Please accept TNC first."
-									}
-								},
-								invalidHandler : function(event, validator) { // display
-																				// error
-																				// alert
-																				// on
-																				// form
-																				// submit
-								},
-								highlight : function(element) { // hightlight
-																// error inputs
-									$(element).closest('.control-group')
-											.addClass('error'); // set error
-																// class to the
-																// control group
-								},
-								success : function(label) {
-									label.closest('.control-group')
-											.removeClass('error');
-									label.remove();
-								},
-								errorPlacement : function(error, element) {
-									if (element.attr("name") == "tnc") { // insert
-																			// checkbox
-																			// errors
-																			// after
-																			// the
-																			// container
-										error
-												.addClass(
-														'help-small no-left-padding')
-												.insertAfter(
-														$('#register_tnc_error'));
-									} else {
-										error
-												.addClass(
-														'help-small no-left-padding')
-												.insertAfter(
-														element
-																.closest('.input-icon'));
-									}
-								},
-								submitHandler : function(form) {
-									window.location.href = "index.html";
-								}
-							});
 			jQuery('#register-btn').click(function() {
 				jQuery('.login-form').hide();
 				jQuery('.register-form').show();
